96c64630a38dd7e81f83ffc8f1103ffc9b5efa90
kenmartey <kenmartey89@gmail.com>
2022-02-24 11:47:26 -0500

    Updated data smart contract and separated filed

---
+++ src/TreeChainToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TreeChainToken is ERC20, Ownable{
+
+    constructor() ERC20("TreeChainToken", "TCT"){}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
