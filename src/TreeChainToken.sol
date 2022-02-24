cf10c4e0d7bc18127a29b76fb408631c673aab6d
kenmartey <kenmartey89@gmail.com>
2022-02-24 17:34:51 -0500

    Added function to mint and withdraw token in token contract

--- src/TreeChainToken.sol
+++ src/TreeChainToken.sol
@@ -7,9 +7,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TreeChainToken is ERC20, Ownable{
 
+    mapping(address => uint256) public balance;
+
     constructor() ERC20("TreeChainToken", "TCT"){}
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function mint(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+        balance[msg.sender] += amount;
     }
+   
+    function getBalance(address _address) public view returns(uint){
+        return balance[_address];
+    }
+
+    function withdraw(address _to, uint _amount) public {
+        balance[msg.sender] -= _amount;
+        balance[_to] += _amount;
+
+    }
+
 }
